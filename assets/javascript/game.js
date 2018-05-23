e10bb367fbc1394df7e7b57aedd022beda11efee
mVang <meng.vang92@yahoo.com>
2018-05-23 18:31:15 -0500

    New

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+var player = 0;
+var cpu = 0;
+
 var wordList = [
     "engine cylinder head",
     "camshaft",
@@ -11,3 +14,10 @@ var wordList = [
     "fan clutch"
 ]
 
+var playerScore =
+    "<p>"+ player + "</p>";
+    document.querySelector("#playerScore").innerHTML = playerScore;
+
+var cpuScore =
+    "<p>"+ cpu + "</p>"
+    document.querySelector("cpuScore").innerHTML = cpuScore;

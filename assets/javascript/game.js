483f88bf5a8b45b88698ed76eede1af7f2fd9399
mVang <meng.vang92@yahoo.com>
2018-05-23 16:35:03 -0500

    More

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wordList = [
+    "engine cylinder head",
+    "camshaft",
+    "crankshaft",
+    "valve cover gasket",
+    "vehicle speed sensor",
+    "drive belt tensioner pulley",
+    "torque converter",
+    "catalytic converter",
+    "diagnostic trouble code reader",
+    "fan clutch"
+]
+

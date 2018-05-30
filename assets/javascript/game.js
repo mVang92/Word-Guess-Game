ba79fd5a3573d84193fdc75bb19d048ccb209689
unknown <mVang92@github.com>
2018-05-30 01:12:57 -0500

    Working kinda

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
 // GLOBAL SCOPE VARIABLES
 // Make an array of words to guess
 var wordList = [
-    "wrench".split(''), 
-    "hammer".split(''), 
-    "screw".split(''), 
-    "distributor".split(''), 
+    "wrench", //.split(''), 
+    "hammer", //.split(''), 
+    "screw", //.split(''), 
+    "distributor", //.split(''), 
 ]
 
 // Randomlly pick a word from wordList array
-var game = 0;
 var randomWord = Math.floor(Math.random() * wordList.length);
 var chosenWord = wordList[randomWord];
 var myLength = chosenWord.length;
 var display = [myLength];
 var win = myLength;
-// Splits answer array into separate characters at each index
-// Use toString method to get underscores to generate correctly
+// // Splits answer array into separate characters at each index
+// // Use toString method to get underscores to generate correctly
 var letters = chosenWord; //.toString().split('');
 var lives = 10;
-// How we display the underscore initially
+var rightGuess = [];
+var wrongGuess = 0;
+// // How we display the underscore initially
 var output = "";
-// What ever letter the user guesses
+// // What ever letter the user guesses
 var userLetter = "";
 // END GLOBAL SCOPE VARIABLES
 
@@ -31,23 +32,20 @@ var running = function() {
 
     // Displays amount of lives left into HTML
     document.getElementById("lives").innerHTML = lives;
-    // Generates underscores for the number of characters in the chosenWord array
+    //Generates underscores for the number of characters in the chosenWord array
     for (var i = 0; i < chosenWord.length; i++){
-        display[i] = "_ ";
+        // Displays underscores onto the screen
+        display[i] = "_,";
         output += display[i];
+        // Put some underscores into rightGuess array
+        rightGuess.push("_")
     }
     document.getElementById("wordToGuess").innerHTML = output;
+    // Resets underscores
     output = "";
-    // Test to see chosen word within this function
-    console.log(chosenWord)
-}
+    // Stop cheating...
+    console.log(chosenWord);
 
-// Guessing function
-var guessing = function(){
-    // Test guessing function
-    // console.log("in guessing function")
-
-    output = "";
     // Listens to key presses
     document.onkeypress = function(event) {
         event = event || window.event;
@@ -57,28 +55,28 @@ var guessing = function(){
         // Test
         // console.log(userLetter);
 
-        for (var n = 0; n < chosenWord.length; n++){
-            // console.log(letters[n]);
-            if (userLetter == letters[n]) {
-                
-                display[n] = userLetter;
-                win--;
-                console.log("if now")
-            }
-            output += display[n];
-        }
-
-        document.getElementById("lives").innerHTML = lives;
-        output = "";
-        lives--;
-
-        if (win < 1) {
-            console.log("win")
-        } else if ( lives < 1) {
-            console.log("lose")
+        if (chosenWord.indexOf(userLetter) < 0) {
+            console.log("Bad guess!");
+            lives--;
+        
+        // If the letter exists in the word, we need to
+        // add it to the good guesses array
         } else {
-            console.log("you have " + lives + " lives remaining.")
+            for (i = 0; i < chosenWord.length; i++) {
+                // Each time the guess letter is found, we
+                // add it as a good guess in the same spot
+                if (chosenWord[i] === userLetter) {
+                    rightGuess[i] = userLetter;
+                    document.getElementById("wordToGuess").innerHTML = rightGuess;
+                }
+            }
         }
+
+        if (chosenWord.indexOf(rightGuess) > 0){
+            console.log("win")
+        }
+        output = "";
+        document.getElementById("lives").innerHTML = lives;
     }
 }
 
@@ -89,79 +87,6 @@ document.getElementById("newBtn").addEventListener("click", function(){
 
     // Reset lives when new button is clicked
     lives = 10;
-    //Calls running and guessing functions
+    //Calls running function
     running();
-    guessing ();
 });
-
-// document.getElementById("newBtn").addEventListener("click", function(){
-//     var makeUnderScore = 0
-//     word = [];
-//     for ( var i = 0; i < chosenWord.length; i++ ){
-//         // Generates underscores and displays them on the screen
-//         word.push(" _ ")
-//         document.getElementById("wordToGuess").innerHTML = word;
-//     }
-    
-//     // Listens to key presses
-//     document.onkeypress = function(event) {
-//         event = event || window.event;
-//         var charCode = event.keyCode || event.which;
-//         var charStr = String.fromCharCode(charCode);
-//         // Test
-//         console.log(charStr);
-//         guess.push(charStr);
-//         console.log(guess);
-            
-//         if (guess.length === chosenWord.length && guess === chosenWord) {
-//             console.log("Good");
-//         }
-//     };
-
-// });
-
-
-// document.addEventListener("keypress", check());
-// function check() {
-//     for (var i = 0; i < chosenWord.length; i++ ) {
-//         if ("keypress" === wordList[i]) {
-//             console.log("keypress")
-//         // rightLetter[i] = guess;
-//         }
-//     }
-// }
-
-// // Create underscores based on length of the chosen word
-// // This makes an array of under scores (underScore)
-// // Using fat arrow (=>) for more concise syntax
-
-
-// console.log(makeUnderScore());
-
-// // Use .addEventListener to track user key input
-// // // Using fat arrow again
-// document.addEventListener("keypress", (event) => {
-//     // This gets the key code of the key being pressed
-//     let keycode = event.keyCode;
-//     // Converts the keycode from a code to a string
-//     // keycode = event.keyCode, so we could also sustitute (keycode) for (event.keyCode), and delete the previous line of code
-//     let keyWord = String.fromCharCode(keycode);
-//     // Use .indexOf method to compare the chosen word
-//     // If user guess is right
-//     if (chosenWord.indexOf(keyWord) > -1) {
-//         // Add to right word array
-//         rightWord.push(keyWord);
-//         // Replace underscore with the right letter
-//         underScore[chosenWord.indexOf(keyWord)] = keyWord;
-//         // Checks to see if user word matches guesses
-//         if (underScore.join(" ") == chosenWord) {
-//             alert("You Win");
-//         }
-//     } else {
-//         // Add to wrong word array
-//         wrongWord.push(keyWord);
-//     }
-    
-// });
-
-// // docUnderScore[0].innerHTML = makeUnderScore().join(" ");
